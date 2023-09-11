a7dec563d7c2be4ea873ea199b135f8da5c655ee
Mohitsarraf28 <mohitsarraf9@gmail.com>
2023-09-11 15:18:59 +0530

    Minor Bugs Fixed

--- script.js
+++ script.js
@@ -11,26 +11,20 @@ document.addEventListener("DOMContentLoaded", function () {
                 ${taskText} <button class="delete">Delete</button>
             `;
 
-            // Apply initial styles for fade-in transition
             li.style.opacity = "0";
             li.style.transform = "translateX(-10px)";
 
-            // Add the item to the list
             taskList.appendChild(li);
             taskInput.value = "";
 
-            // Trigger the browser reflow to apply the initial styles
             li.getBoundingClientRect();
 
-            // Apply fade-in animation
             li.style.transition = "opacity 0.3s ease, transform 0.3s ease";
             li.style.opacity = "1";
             li.style.transform = "translateX(0)";
 
-            // Delete button functionality
             const deleteButton = li.querySelector(".delete");
             deleteButton.addEventListener("click", function () {
-                // Apply fade-out animation
                 li.style.transition = "opacity 0.3s ease, transform 0.3s ease";
                 li.style.opacity = "0";
                 li.style.transform = "translateX(-10px)";
